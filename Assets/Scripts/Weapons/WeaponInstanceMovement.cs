d030ee4e8c8af7461f562397ba65bd70ef738c9b
Spectralle <tnvpetersen@yahoo.com.au>
2021-06-21 23:05:56 +1000

    Lasers destroy enemies and themselves on hit

--- Assets/Scripts/Weapons/WeaponInstanceMovement.cs
+++ Assets/Scripts/Weapons/WeaponInstanceMovement.cs
@@ -9,4 +9,13 @@ public class WeaponInstanceMovement : MonoBehaviour
     void Awake() => Destroy(gameObject, Lifetime);
 
     private void Update() => transform.Translate(Vector3.up * Speed * Time.deltaTime);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
 }
