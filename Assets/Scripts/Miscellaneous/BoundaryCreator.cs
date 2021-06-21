2193ec8499a47c5f7e7ac200769ccaaafeb61e49
Spectralle <tnvpetersen@yahoo.com.au>
2021-06-21 22:07:19 +1000

    Created a LevelBoundary static system in its own script, and an enemy with simple vertical movement

---
+++ Assets/Scripts/Miscellaneous/BoundaryCreator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+public class BoundaryCreator : MonoBehaviour
+{
+    [SerializeField] private Bounds levelBounds;
+
+
+    void Awake() => LevelBoundary.SetBounds(levelBounds);
+
+
+    private void OnDrawGizmos()
+    {
+        Vector3 topLeft = new Vector3(levelBounds.center.x - levelBounds.extents.x, levelBounds.center.y + levelBounds.extents.y, 0);
+        Vector3 topRight = new Vector3(levelBounds.center.x + levelBounds.extents.x, levelBounds.center.y + levelBounds.extents.y, 0);
+        Vector3 bottomLeft = new Vector3(levelBounds.center.x - levelBounds.extents.x, levelBounds.center.y - levelBounds.extents.y, 0);
+        Vector3 bottomRight = new Vector3(levelBounds.center.x + levelBounds.extents.x, levelBounds.center.y - levelBounds.extents.y, 0);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(topLeft, bottomLeft);
+        Gizmos.DrawLine(bottomLeft, bottomRight);
+        Gizmos.DrawLine(topRight, bottomRight);
+    }
+}
---
+++ Assets/Scripts/Miscellaneous/LevelBoundary.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+
+public static class LevelBoundary
+{
+    private static Bounds _bounds;
+
+
+    static LevelBoundary() => _bounds = new Bounds(Vector3.zero, Vector3.one);
+
+    #region Sets
+    public static void SetBounds(Vector2 radius) => _bounds.extents = radius;
+
+    public static void SetCenter(Vector2 center) => _bounds.center = center;
+
+    public static void SetBounds(Bounds bounds) => _bounds = bounds;
+    #endregion
+
+    #region Gets
+    public static Vector2 GetRadius() => _bounds.extents;
+
+    public static Vector2 GetCenter() => _bounds.center;
+
+    public static Bounds GetBounds() => _bounds;
+    #endregion
+
+    #region Get Points
+    public static float L() => _bounds.center.x - _bounds.extents.x;
+    public static float L(float offset) => _bounds.center.x - _bounds.extents.x + offset;
+    public static float R() => _bounds.center.x + _bounds.extents.x;
+    public static float R(float offset) => _bounds.center.x + _bounds.extents.x + offset;
+    public static float U() => _bounds.center.y + _bounds.extents.y;
+    public static float U(float offset) => _bounds.center.y + _bounds.extents.y + offset;
+    public static float D() => _bounds.center.y - _bounds.extents.y;
+    public static float D(float offset) => _bounds.center.y - _bounds.extents.y + offset;
+    #endregion
+}
