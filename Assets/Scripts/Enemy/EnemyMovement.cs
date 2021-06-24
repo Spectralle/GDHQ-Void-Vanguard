0807e949d1338065b330270993f8ed646d3d3774
Spectralle <tnvpetersen@yahoo.com.au>
2021-06-25 03:47:19 +1000

    Converted Player, Laser, and Enemy to 2D objects

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -19,7 +19,7 @@ public class EnemyMovement : MonoBehaviour
 
     private void OnDestroy() => EnemySpawnManager.EnemiesAlive--;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Projectile"))
         {
